3ee095bf3df5b6a3c6b24fa5657765208fb1f2a9
zzcode <6apmuk@gmail.com>
2020-06-28 18:13:23 +0300

    Commit

--- Lessons/classificat-dinamyk/script.js
+++ Lessons/classificat-dinamyk/script.js
@@ -17,3 +17,30 @@ console.log(persone.name);
 
 let images = ['red.jpg', 'green.jpg', 'yellow.png'];
 console.log(images[0]);
+
+
+console.log(typeof(String(4)));
+console.log(typeof('www' + 5));
+console.log(typeof(+'12'));
+console.log(typeof(5 + +'5'));
+console.log(typeof(parseInt('15px', 10)));
+
+let answer = +prompt('Age 18?', 'Yes');
+
+
+// let switcher = null;
+
+// if (switcher) {
+    // console.log('Not see you')
+// };
+
+
+let switcher = 1;
+
+if (switcher) {
+    console.log('See you')
+};
+
+
+console.log(typeof(Boolean('5')));
+console.log(typeof(!!('5')));
