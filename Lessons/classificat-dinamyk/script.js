31575aef7f57bc0e87804bcc56a834b6ea4e9244
zzcode <6apmuk@gmail.com>
2020-06-28 17:53:09 +0300

    Commit Lessons

---
+++ Lessons/classificat-dinamyk/script.js
@@ -0,0 +1,19 @@
+var number = 5;
+var string = 'Hello!';
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+let persone = {
+    name: 'ALex',
+    age: 37,
+    city: 'Moscow'
+};
+console.log(persone.name);
+
+
+let images = ['red.jpg', 'green.jpg', 'yellow.png'];
+console.log(images[0]);
