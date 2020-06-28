da3f78bda633fe1a46e8343138bd800e0905332a
zzcode <6apmuk@gmail.com>
2020-06-28 11:02:59 +0300

    This is our first comit

---
+++ lesson1/script.js
@@ -0,0 +1,13 @@
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++ );
+console.log(decr--);
+
+console.log(5%2);
+
+let isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose)
