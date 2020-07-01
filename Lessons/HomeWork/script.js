c6c68e0220be1b76e2b3a4354d3dc8714da9f189
zzcode <6apmuk@gmail.com>
2020-07-01 12:50:08 +0300

    commit

---
+++ Lessons/HomeWork/script.js
@@ -0,0 +1,39 @@
+
+let money = +prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+
+let appData = {
+    budjet: money,
+    expenses: {},
+    timeData: time,
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Укажите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдётся?', '');
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        
+    }
+};
+
+appData.moneyPerDay = appData.budjet / 30;
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка')
+} else {
+    console.log('Произошла ошибка');
+}
---
+++ Lessons/tsikli/script.js
@@ -0,0 +1,24 @@
+let num = 50;
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        break;
+    }
+    console.log(i);
+}
+
+
+
