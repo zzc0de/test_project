74b157b1c2d528b07566f332bee10b344a6efd59
zzcode <6apmuk@gmail.com>
2020-07-05 14:19:47 +0300

    Commit

--- Lessons/funkcii/script.js
+++ Lessons/funkcii/script.js
@@ -39,6 +39,37 @@ console.log(parseInt(twelve));
 console.log(parseFloat(twelve));
 
 
+function sqr(x) {
+    return x*x;
+}
+console.log(sqr(10));
+
+
+const sq = (x, y) => x*y;
+console.log(sq(4, 5));
+
+
+const arr = [23, 45, 66, 78, 99, 101];
+
+const res = arr
+    .map((el) => parseInt(el))
+    .filter((num) => num%2)
+    .reduce((max, value) => Math.max(max, value), 0);
+console.log(res);
+
+
+const greeter = {
+    greet: function (name) {
+        console.log('Hello', name);
+    },
+
+    greetAll: function (names) {
+        names.forEach((name) => {
+            this.greet(name);
+        });
+    }
+};
+greeter.greetAll(['Bob', 'Mark', 'Peter']);
 
 
 
