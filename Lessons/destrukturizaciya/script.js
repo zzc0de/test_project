c132c52835599923c3ee07a44ec58dac401c9b6a
zzcode <6apmuk@gmail.com>
2020-07-06 15:28:45 +0300

    commit

--- Lessons/destrukturizaciya/script.js
+++ Lessons/destrukturizaciya/script.js
@@ -1,3 +1,5 @@
+// деструкторизация для объектоа
+
 const person = {
     firstName: 'Alex',
     lastName: 'Bart',
@@ -35,3 +37,33 @@ connect();
 connect({port: 1111});
 
 
+const dict = {
+    dog: 'gavgav',
+    elephan: 'uuuuu',
+    mouse: 'piiii'
+}
+const {dog, ...otherAnimals } = dict;
+console.log(otherAnimals);
+
+// деструкторизация для массивов
+
+const fib = [1, 2, 3, 4, 5, 6, 7,];
+const [a, b, c] = fib;
+console.log(a, b, c);
+
+const fib = [1, 2, 3, 4, 5, 6, 7,];
+const [, a, , b] = fib;
+console.log(a, b);
+
+const line = [[10, 12], [33, 57]];
+const [[p1x, p1y], [p2x, p2y]] = line;
+console.log(p1x, p1y, p2x, p2y);
+
+const people = ['alex', 'sandra'];
+const [a, b, c = 'another'] = people;
+console.log(a, b, c);
+
+
+const people = ['chris', 'sandra', 'bob', 'alex'];
+const [a, ...others] = people;
+console.log(others);
