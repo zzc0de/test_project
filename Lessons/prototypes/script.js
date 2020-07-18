82f74ccd170a0cad094c33089048706ef7763729
zzcode <6apmuk@gmail.com>
2020-07-18 22:26:31 +0300

    commit

--- Lessons/prototypes/script.js
+++ Lessons/prototypes/script.js
@@ -1,3 +1,49 @@
+//--- Object.set.PrototypeOf
+
+const animal = {
+    say: function() {
+        console.log(this.name, 'says', this.voice);
+    }
+};
+
+const dog = {
+    name: 'dog',
+    voice: 'Gav!',
+};
+Object.setPrototypeOf(dog, animal);
+
+const cat = {
+    name: 'cat',
+    voice: 'Myau!',
+};
+Object.setPrototypeOf(cat, animal);
+
+dog.say();
+cat.say();
+
+//--- Object.create
+
+const animal = {
+    say: function() {
+        console.log(this.name, 'says', this.voice);
+    }
+};
+
+function createAnimal(name, voice) {
+    const result = Object.create(animal);
+    result.name = name;
+    result.voice = voice;
+    return result;
+}
+
+const dog = createAnimal('dog', 'wuuf!');
+const cat = createAnimal('cat', 'myauu!');
+
+dog.say();
+cat.say();
+
+//--- using new
+
 function Animal(name, voice) {
     this.name = name;
     this.voice = voice;
--- REACT/todo/src/index.js
+++ REACT/todo/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const ToDoList = () => {
+    return (
+      <ul>
+        <li>Learn React</li>
+        <li>Build Awesome APP</li>
+      </ul>
+    );
+};
+
+const AppHeader = () => {
+  return (
+    <h1>My TodoList</h1>
+  );
+};
+
+const SearchPanel = () => {
+  return (
+    <input placeholder='Searching' />
+  );
+};
+
+const el = (
+<div>
+  <AppHeader />
+  <SearchPanel />
+  <ToDoList />
+</div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(el, document.getElementById('root'));
